e05d5a2a63a5b4e17f5fe5514a0e08a09704755b
heehyun0221 <psr4580@gmail.com>
2025-01-03 13:47:57 +0900

    Quiz : Fetch join

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -4,9 +4,12 @@ import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
+  @Query(value = "SELECT t FROM Todo t JOIN FETCH t.user u ORDER BY t.modifiedAt DESC"
+      ,countQuery = "SELECT COUNT(t) FROM Todo t")
   Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
   int countById(Long todoId);
